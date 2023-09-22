3c09f5b465d966e9d99bc4bb0399f0d984e718e6
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-09-21 23:25:53 -0300

    commit 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // Usar o darkMode como 'class'
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
