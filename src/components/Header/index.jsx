bb2403b6f731f8d9d73e8a4b7ed8d47b8d3b4b76
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-09-21 21:57:44 -0300

    commit 2

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,12 @@
+import { MoonIcon } from "@heroicons/react/24/solid";
+
 const Header = () => {
   return (
     <div className="flex h-20 bg-gray-500 justify-between items-center px-5">
       <span className="text-gray-100 text-xl">Olá, usuario</span>
       <h1>Alura Newsletter</h1>
-      <input className="hidden sm:block" type="checkbox" />
+      {/* <input className="hidden sm:block" type="checkbox" /> */}
+      <MoonIcon className="h-8 to-gray-100 hidden sm:block" />
     </div>
   );
 }
