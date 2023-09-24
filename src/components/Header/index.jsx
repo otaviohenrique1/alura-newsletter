7e1ed81455c73d621aea4f82974a615fd8262b4a
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-09-24 14:47:17 -0300

    commit 5

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,10 +2,9 @@ import ToggleTheme from "../ToggleTheme/ToggleTheme";
 
 const Header = () => {
   return (
-    <div className="flex h-20 bg-gray-500 justify-between items-center px-5">
+    <div className="flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5">
       <span className="text-gray-100 text-xl">Olá, usuario</span>
       <h1 className="text-gray-100 text-xl">Alura Newsletter</h1>
-      {/* <input className="hidden sm:block" type="checkbox" /> */}
       <ToggleTheme />
     </div>
   );
--- src/components/ToggleTheme/ToggleTheme.jsx
+++ src/components/ToggleTheme/ToggleTheme.jsx
@@ -1,6 +1,13 @@
 import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+import { useEffect } from "react";
 
 const ToggleTheme = () => {
+  const systemPreference = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+  useEffect(() => {
+    systemPreference && document.documentElement.classList.add('dark')
+  });
+
   const toggle = () => {
     document.documentElement.classList.toggle('dark')
   }
