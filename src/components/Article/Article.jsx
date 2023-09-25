39d591ea7b5a4f2a0a25f7170e3f23ca8d290700
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-09-24 21:22:25 -0300

    commit 8

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,8 +1,20 @@
 /* eslint-disable react/prop-types */
 const Article = ({ title, text, tags, image, alt }) => {
   return (
-    <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex flex-col items-center">
+    <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg gap-2 flex flex-col items-center">
       <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold">{title}</h3>
+      <div className="w-full flex flex-row justify-end gap-2 pr-5">
+        {
+          tags.map((tag) => {
+            return (
+              <span
+                key={tag}
+                className="bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full text-gray-200 text-xs font-bold uppercase"
+              >{tag}</span>
+            );
+          })
+        }
+      </div>
       <div className="grid gap-1">
         {
           text.map((content, index) => {
@@ -15,13 +27,8 @@ const Article = ({ title, text, tags, image, alt }) => {
           })
         }
       </div>
-      {(image) && (
-        <img
-          className="sm:p-4"
-          src={image}
-          alt={alt}
-        />
-      )}
+      {image && <img className="sm:p-4" src={image} /> }
+      {image && alt && <span className="sr-only">{alt}</span>}
     </div>
   );
 }
--- src/components/ArticlesList/ArticlesList.jsx
+++ src/components/ArticlesList/ArticlesList.jsx
@@ -4,7 +4,7 @@ import Article from "../Article/Article";
 
 const ArticlesList = () => {
   return (
-    <div className="mt-5 sm:mt-0 grid gap-5 m-auto max-w-2xl sm:grid-cols-2 lg:grid-cols-3 lg:max-w-[1000px]">
+    <div className="mt-5 sm:mt-2 grid gap-5 m-auto max-w-2xl sm:grid-cols-2 lg:grid-cols-3 lg:max-w-[1000px]">
       {data.map((props, index) => {
         return (
           <Article key={index} {...props}/>
