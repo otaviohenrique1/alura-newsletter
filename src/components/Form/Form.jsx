6fcba7d7bf5325f69b579f38fcb2c5d76954a1e3
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-09-26 12:56:12 -0300

    commit 11

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,3 +1,5 @@
+import Input from "../Input/Input";
+
 /* eslint-disable react/prop-types */
 const Form = ({ onSubmit }) => {
   const safeSubmit = event => {
@@ -11,11 +13,11 @@ const Form = ({ onSubmit }) => {
   return (
     <form
       onSubmit={safeSubmit}
-      className="h-full flex flex-col items-center justify-center"
+      className="h-full flex flex-col items-center justify-center gap-10 -mt-20 mx-5"
     >
-      <input type="text" placeholder="Insira seu nome..." />
-      <input type="email" placeholder="Insira seu email..." />
-      <button type="submit">Seguir</button>
+      <Input type="text" required placeholder="Insira seu nome..." />
+      <Input type="email" required placeholder="Insira seu email..." />
+      <button type="submit" className="py-1 px-5 max-w-sm bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 uppercase outline-none">Seguir</button>
     </form>
   );
 }
---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,7 @@
+const Input = (props) => {
+  return (
+    <input className="py-1 pl-5 max-w-lg w-full dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full outline-none" {...props} />
+  );
+}
+
+export default Input;
